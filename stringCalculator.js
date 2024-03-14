b8b0d3a6762a1ebe910affe8c7e58edd48e0a89e
Ayushi Agrawal <ayushiagrawal0501@gmail.com>
2024-03-14 20:00:36 +0530

    Added code for calculating the sum of string and added test cases for the same

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,10 @@ function calcString(num) {
     if (num === "") {
         return 0;
     }
+    
+    const numsArray = num.split(/,|\n/);
+
+    return numsArray.reduce((acc, curr) => acc + parseInt(curr), 0);
 }
 
 module.exports = calcString;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,4 +1,6 @@
 const calcString = require('./stringCalculator');
 test('Red Phase', ()=> {
     expect(calcString("")).toBe(0);
+    expect(calcString("1\n2,3")).toBe(6);
+    expect(calcString("1,5")).toBe(6);
 });
