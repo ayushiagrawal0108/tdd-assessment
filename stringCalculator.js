6655bc6b094488dc5044846acc71ffd9fa757823
Ayushi Agrawal <ayushiagrawal0501@gmail.com>
2024-03-14 20:43:03 +0530

    Added negetive number and delimeter logic

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,11 +1,31 @@
 function calcString(num) {
     if (num === "") {
-        return 0;
+      return 0;
     }
-    
-    const numsArray = num.split(/,|\n/);
-
-    return numsArray.reduce((acc, curr) => acc + parseInt(curr), 0);
-}
-
-module.exports = calcString;
+  
+    let delimiterPattern = /,|\n/; // Default delimiter pattern
+  
+    // Check if the input string starts with "//"
+    if (num.startsWith("//")) {
+      // Extract custom delimiter
+      const delimiterEndIndex = num.indexOf("\n");
+      const customDelimiter = num.substring(2, delimiterEndIndex);
+      // Update delimiter pattern with custom delimiter
+      delimiterPattern = new RegExp(`[${customDelimiter}]`);
+      // Remove custom delimiter definition line from input string
+      num = num.substring(delimiterEndIndex + 1);
+    }
+  
+    const numsArray = num.split(delimiterPattern).map(Number);
+  
+  
+    const negativeNumbers = numsArray.filter(num => num < 0);
+  
+    if (negativeNumbers.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(",")}`);
+    }
+  
+    return numsArray.reduce((acc, curr) => acc + curr, 0);
+  }
+  
+  module.exports = calcString;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,6 +1,17 @@
 const calcString = require('./stringCalculator');
-test('Red Phase', ()=> {
-    expect(calcString("")).toBe(0);
-    expect(calcString("1\n2,3")).toBe(6);
-    expect(calcString("1,5")).toBe(6);
+
+test('Custom Delimiter and Negative Numbers', () => {
+
+  expect(calcString("//@\n1@2@3")).toBe(6); 
+
+
+  expect(() => calcString("//@\n-1@2@-3")).toThrow("Negative numbers not allowed: -1,-3");
+
+  expect(() => calcString("1,-2,3")).toThrow("Negative numbers not allowed: -2");
+
+  expect(calcString("")).toBe(0);
+
+  expect(calcString("5")).toBe(5);
+
+  expect(calcString("//;\n1;2")).toBe(3); // Custom delimiter is ";"
 });
